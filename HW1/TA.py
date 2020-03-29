fix hw1 by normalizing H

HW1/TA.py
@@ -18,7 +18,7 @@ objpoints = [] # 3d points in real world space
 imgpoints = [] # 2d points in image plane.
 
 # Make a list of calibration images
-images = glob.glob('ourdata/*.jpg')
+images = glob.glob('data/*.jpg')
 
 # Step through the list and search for chessboard corners
 print('Start finding chessboard corners...')
@@ -55,88 +55,17 @@ img_size = (img.shape[1], img.shape[0])
 # You need to comment these functions and write your calibration function from scratch.
 # Notice that rvecs is rotation vector, not the rotation matrix, and tvecs is translation vector.
 # In practice, you'll derive extrinsics matrixes directly. The shape must be [pts_num,3,4], and use them to plot.
-'''if using opencv function:
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size,None,None)
 Vr = np.array(rvecs)
 Tr = np.array(tvecs)
 extrinsics = np.concatenate((Vr, Tr), axis=1).reshape(-1,6)
-'''
-
 """
-get homography H_list from all images
-solve P@m=0
-    P: (2*#image,9) ndarray
-    m: (9,1) ndarray
-"""
-H_list=[]   # 10*(3*3)
-for pic in range(len(images)):
-    P=np.zeros((corner_x*corner_y*2,9),np.float32)
-    for i in range(corner_x*corner_y):
-        row=i*2
-        P[row,:3]=P[row,-3:]=P[row+1,3:6]=P[row+1,-3:]=np.append(objpoints[pic][i,:-1],1.)
-        P[row,-3:]*=-imgpoints[pic][i,0,0]
-        P[row+1,-3:]*=-imgpoints[pic][i,0,1]
-    _,_,V=np.linalg.svd(P)
-    H=V[-1,:]/V[-1,-1]  # normalize, so H[-1,-1]=1.0
-    H=H.reshape((3,3))
-    H_list.append(H)
-
-"""
-get B from H
-solve V@b=0
-    V: (2*#image,6) ndarray
-    b: (6,1) ndarray 
-"""
-V=np.zeros((len(images)*2,6),np.float32)
-for pic in range(len(images)):
-    H=H_list[pic]
-    V[pic*2,:]=np.array([H[0,0]*H[0,1],
-                         H[0,0]*H[1,1]+H[0,1]*H[1,0],
-                         H[0,0]*H[2,1]+H[2,0]*H[0,1],
-                         H[1,0]*H[1,1],
-                         H[1,0]*H[2,1]+H[2,0]*H[1,1],
-                         H[2,0]*H[2,1]],np.float32)
-    V[pic*2+1,:]=np.array([H[0,0]**2-H[0,1]**2,
-                           2*(H[0,0]*H[1,0]-H[0,1]*H[1,1]),
-                           2*(H[0,0]*H[2,0]-H[0,1]*H[2,1]),
-                           H[1,0]**2-H[1,1]**2,
-                           2*(H[1,0]*H[2,0]-H[1,1]*H[2,1]),
-                           H[2,0]**2-H[2,1]**2
-                           ],np.float32)
-_,_,V=np.linalg.svd(V)
-B=V[-1,:]
-B=np.array([[B[0],B[1],B[2]],
-            [B[1],B[3],B[4]],
-            [B[2],B[4],B[5]]],np.float32)
-# turn B into positive definite
-if B[0,0]<0:
-    B=-B
-
-"""
-get K from B
-"""
-K_inv=np.linalg.cholesky(B).T
-K=np.linalg.inv(K_inv)
-K/=K[-1,-1] # normalize, so K[-1,-1]=1.0
-
-"""
-get r1,r2,t from K
-"""
-extrinsics=np.zeros((len(images),6))
-for i in range(len(H_list)):
-    H=H_list[i]
-    _lambda=1/np.linalg.norm(K_inv@H[:,0:1])
-    ex=_lambda*K_inv@H
-    r1=ex[:,0:1]
-    r2=ex[:,1:2]
-    r3=np.cross(r1.T,r2.T).T
-
-    R=np.hstack((r1, r2, r3))
-    t=ex[:,2:3]
-    rot_vec,_=cv2.Rodrigues(R)
-    extrinsics[i,:]=np.concatenate((rot_vec,t)).reshape(-1)
+Write your code here
 
 
+
+
+"""
 # show the camera extrinsics
 print('Show the camera extrinsics')
 # plot setting
@@ -144,7 +73,7 @@ print('Show the camera extrinsics')
 fig = plt.figure(figsize=(10, 10))
 ax = fig.gca(projection='3d')
 # camera setting
-camera_matrix = K
+camera_matrix = mtx
 cam_width = 0.064/0.1
 cam_height = 0.032/0.1
 scale_focal = 1600
@@ -180,6 +109,7 @@ ax.set_zlabel('-y')
 ax.set_title('Extrinsic Parameters Visualization')
 plt.show()
 plt.pause(1)
+
 #animation for rotating plot
 """
 for angle in range(0, 360):