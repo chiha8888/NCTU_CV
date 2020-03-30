0afbd57cefede49f8c3092dab47d5b9cdfba00aa
dora wu <chiha8888@gmail.com>
2020-03-30 10:40:39 +0800

    add hw1 report.pdf

--- HW1/camera_calibration.py
+++ HW1/camera_calibration.py
@@ -18,7 +18,7 @@ objpoints = [] # 3d points in real world space
 imgpoints = [] # 2d points in image plane.
 
 # Make a list of calibration images
-images = glob.glob('ourdata/*.jpg')
+images = glob.glob('data/*.jpg')
 
 # Step through the list and search for chessboard corners
 print('Start finding chessboard corners...')
